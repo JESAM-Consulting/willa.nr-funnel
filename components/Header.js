37efc5ee572f4ca96be257be454da91db589c579
kevin <kevin088.rejoice@gmail.com>
2023-03-25 12:26:43 +0530

    Changed head title

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,7 @@ import Head from "next/head";
 export default function Header() {
   return (
     <Head>
-      <title>willa.nr</title>
+      <title>willa.nr.8</title>
       <meta
         name="description"
         content="willa.nr.8 - individuelle Solar-Beratung."
